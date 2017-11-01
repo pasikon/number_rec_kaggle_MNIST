37e5c879d53bc8ab306712c34cee582d6960fb13
mzurek <michal.zurek@williamhill.com>
2017-11-01 13:37:14 +0100

    added dropout to one layer

--- test/recg.py
+++ test/recg.py
@@ -42,7 +42,8 @@ def cost(logits, labels):
 def create_placeholders(n_x, n_y):
     X = tf.placeholder(tf.float32, shape=(n_x, None), name="X")
     Y = tf.placeholder(tf.float32, shape=(n_y, None), name="Y")
-    return X, Y
+    keepprob_A1 = tf.placeholder(tf.float32)
+    return X, Y, keepprob_A1
 
 
 def initialize_parameters():
@@ -63,7 +64,7 @@ def initialize_parameters():
     return parameters
 
 
-def forward_propagation(X, parameters):
+def forward_propagation(X, parameters, keepprob_A1):
     W1 = parameters['W1']
     b1 = parameters['b1']
     W2 = parameters['W2']
@@ -73,8 +74,11 @@ def forward_propagation(X, parameters):
 
     Z1 = tf.matmul(W1, X) + b1  # Z1 = np.dot(W1, X) + b1
     A1 = tf.nn.relu(Z1)  # A1 = relu(Z1)
-    Z2 = tf.matmul(W2, A1) + b2  # Z2 = np.dot(W2, a1) + b2
+    relu1_dropout = tf.nn.dropout(A1, keep_prob=keepprob_A1)
+
+    Z2 = tf.matmul(W2, relu1_dropout) + b2  # Z2 = np.dot(W2, a1) + b2
     A2 = tf.nn.relu(Z2)  # A2 = relu(Z2)
+
     Z3 = tf.matmul(W3, A2) + b3  # Z3 = np.dot(W3,Z2) + b3
 
     return Z3
@@ -88,29 +92,16 @@ def compute_cost(Z3, Y):
 
 
 def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
-    """
-    Creates a list of random minibatches from (X, Y)
 
-    Arguments:
-    X -- input data, of shape (input size, number of examples)
-    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
-    mini_batch_size - size of the mini-batches, integer
-    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
-
-    Returns:
-    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
-    """
-
-    m = X.shape[1]  # number of training examples
+    m = X.shape[1]
     mini_batches = []
-    np.random.seed(seed)
 
-    # Step 1: Shuffle (X, Y)
+    # Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
     shuffled_X = X[:, permutation]
     shuffled_Y = Y[:, permutation].reshape((Y.shape[0], m))
 
-    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    # Partition (shuffled_X, shuffled_Y). Minus the end case.
     num_complete_minibatches = math.floor(
         m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
     for k in range(0, num_complete_minibatches):
@@ -119,7 +110,7 @@ def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
 
-    # Handling the end case (last mini-batch < mini_batch_size)
+    # Handling end case where last mini-batch < mini_batch_size
     if m % mini_batch_size != 0:
         mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size: m]
         mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size: m]
@@ -155,13 +146,13 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
     costs = []  # To keep track of the cost
 
     # Create Placeholders of shape (n_x, n_y)
-    X, Y = create_placeholders(n_x, n_y)
+    X, Y, keepprob_A1 = create_placeholders(n_x, n_y)
 
     # Initialize parameters
     parameters = initialize_parameters()
 
     # Forward propagation
-    Z3 = forward_propagation(X, parameters)
+    Z3 = forward_propagation(X, parameters, keepprob_A1)
 
     # Cost function: Add cost function to tensorflow graph
     cost = compute_cost(Z3, Y)
@@ -192,22 +183,22 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
 
                 # IMPORTANT: The line that runs the graph on a minibatch. Run the session to execute the "optimizer"
                 # and the "cost", the feedict should contain a minibatch for (X,Y).
-                _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+                _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y, keepprob_A1: 0.7})
 
                 epoch_cost += minibatch_cost / num_minibatches
 
             # Print the cost every epoch
-            if print_cost == True and epoch % 100 == 0:
+            if print_cost == True and epoch % 10 == 0:
                 print("Cost after epoch %i: %f" % (epoch, epoch_cost))
             if print_cost == True and epoch % 5 == 0:
                 costs.append(epoch_cost)
 
         # plot the cost
-        plt.plot(np.squeeze(costs))
-        plt.ylabel('cost')
-        plt.xlabel('iterations (per tens)')
-        plt.title("Learning rate =" + str(learning_rate))
-        plt.show()
+        # plt.plot(np.squeeze(costs))
+        # plt.ylabel('cost')
+        # plt.xlabel('iterations (per tens)')
+        # plt.title("Learning rate =" + str(learning_rate))
+        # plt.show()
 
         # lets save the parameters in a variable
         parameters = sess.run(parameters)
@@ -219,8 +210,8 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
         # Calculate accuracy on the test set
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-        print("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
-        print("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+        print("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train, keepprob_A1: 1}))
+        print("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test, keepprob_A1: 1}))
 
         return parameters
 
@@ -269,4 +260,4 @@ print('Y_train shape:' + str(Y_train.shape))
 print('X_test shape:' + str(X_test.shape))
 print('Y_test shape:' + str(Y_test.shape))
 
-parameters = model(X_train, Y_train, X_test, Y_test, num_epochs=50)
+parameters = model(X_train, Y_train, X_test, Y_test, num_epochs=550)
