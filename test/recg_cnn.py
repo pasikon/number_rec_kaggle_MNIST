07212bec53cede1a87c6c9c54b0ed1447d01fdad
michalz <michal.zurek7@gmail.com>
2017-11-06 21:38:00 +0100

    added cnn script version with checkpoint saving

---
+++ test/recg_cnn.py
@@ -0,0 +1,143 @@
+import math
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.framework import ops
+
+from test.imgload.imgload import loaddta
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from test.convolutional.convlayers import bias_variable
+from test.convolutional.convlayers import conv2d
+from test.convolutional.convlayers import conv_layer
+from test.convolutional.convlayers import full_layer
+from test.convolutional.convlayers import max_pool_2x2
+
+def display(img):
+    # (784) => (28,28)
+    one_image = img.reshape(28, 28)
+    plt.axis('off')
+    plt.imshow(one_image, cmap=cm.binary)
+    plt.show()
+
+
+X_loaded, Y_loaded = loaddta('../input/train.csv')
+
+X_train = X_loaded[:, 0:39000]
+Y_train = Y_loaded[:, 0:39000]
+X_test = X_loaded[:, 39001:41999]
+Y_test = Y_loaded[:, 39001:41999]
+
+print('X_train shape:' + str(X_train.shape))
+print('Y_train shape:' + str(Y_train.shape))
+print('X_test shape:' + str(X_test.shape))
+print('Y_test shape:' + str(Y_test.shape))
+
+
+
+#display(X_train[:, 3905])
+
+
+x = tf.placeholder(tf.float32, shape=(None, 784))
+y_ = tf.placeholder(tf.float32, shape=(None, 10))
+
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+conv1 = conv_layer(x_image, shape=[5, 5, 1, 32])
+conv1_pool = max_pool_2x2(conv1)
+
+conv2 = conv_layer(conv1_pool, shape=[5, 5, 32, 64])
+conv2_pool = max_pool_2x2(conv2)
+
+conv2_flat = tf.reshape(conv2_pool, [-1, 7*7*64])
+full_1 = tf.nn.relu(full_layer(conv2_flat, 1024))
+
+keep_prob = tf.placeholder(tf.float32)
+full1_drop = tf.nn.dropout(full_1, keep_prob=keep_prob)
+
+y_conv = full_layer(full1_drop, 10)
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+print("conv1: " + str(conv1))
+print("conv1_pool: " + str(conv1_pool))
+print("conv2: " + str(conv2))
+print("conv2_pool: " + str(conv2_pool))
+print("conv2_flat: " + str(conv2_flat))
+print("full_1: " + str(full_1))
+print("y_conv: " + str(y_conv))
+
+
+def random_mini_batches(X, Y, mini_batch_size=64):
+    """
+    Produces shuffled list of minibatches
+    :param X:
+    :param Y:
+    :param mini_batch_size:
+    :return: List of tuples (mini_batch_X, mini_batch_Y)
+    """
+    m = X.shape[1]
+    mini_batches = []
+
+    # Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((Y.shape[0], m))
+
+    # Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(
+        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # Handling end case where last mini-batch < mini_batch_size
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size: m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size: m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+
+saver = tf.train.Saver(save_relative_paths=True, max_to_keep=20)
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    step = 0
+    for epoch in range(100):
+        epoch_cost = 0.
+        m = X_train.shape[1]
+        minibatch_size = 128
+        num_minibatches = int(m / minibatch_size)
+        minibatches = random_mini_batches(X_train, Y_train, minibatch_size)
+        
+        for minibatch in minibatches:
+            xt = minibatch[0].T
+            yt = minibatch[1].T
+            xt = xt.astype(np.float32)
+            step = step + 1
+            sess.run(train_step, feed_dict={x: xt, y_: yt, keep_prob: 0.48})
+        
+        if epoch % 5 == 0:
+            dev_accuracy = sess.run(accuracy, feed_dict={x: X_test.T, y_: Y_test.T, keep_prob: 1.0})
+            print("EPOCH {}, step {}, dev accuracy {}".format(epoch, step, dev_accuracy))
+            train_accuracy = sess.run(accuracy, feed_dict={x: X_train.T, y_: Y_train.T, keep_prob: 1.0})
+            print("EPOCH {}, training accuracy {}".format(epoch, train_accuracy))
+            saver.save(sess=sess, save_path=os.path.join("../modelz/", "model_chkp"), global_step=step)
+            
+            
+            
+        
+
