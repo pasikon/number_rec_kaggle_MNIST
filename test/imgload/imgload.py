7ef3ffbf72444e9ca29c095c19ca5f271bf6508f
michal <michal.zurek7@gmail.com>
2017-11-19 12:35:46 +0100

    visualization

--- test/imgload/imgload.py
+++ test/imgload/imgload.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 import tensorflow as tf
 import numpy as np
@@ -53,3 +55,38 @@ def one_hot_matrix(labels, C):
         yoh = sess.run(oht)
     yoh = yoh[0]
     return yoh
+
+
+def random_mini_batches(X, Y, mini_batch_size=64):
+    """
+    Produces shuffled list of minibatches
+    :param X:
+    :param Y:
+    :param mini_batch_size:
+    :return: List of tuples (mini_batch_X, mini_batch_Y)
+    """
+    m = X.shape[1]
+    mini_batches = []
+
+    # Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((Y.shape[0], m))
+
+    # Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(
+        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # Handling end case where last mini-batch < mini_batch_size
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size: m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size: m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
--- test/recg_cnn.py
+++ test/recg_cnn.py
@@ -1,32 +1,18 @@
-import math
 import os
 import sys
 import inspect
 
 import tensorflow as tf
-import numpy as np
-from tensorflow.contrib.learn.python.learn.monitors import ValidationMonitor
-from tensorflow.python.estimator.model_fn import EstimatorSpec
-from tensorflow.python.framework import ops
-
-
-from tensorflow.python.training.basic_session_run_hooks import LoggingTensorHook
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from test.imgload.imgload import loaddta
+from test.imgload.imgload import random_mini_batches
 from test.imgload.imgload import load_train_and_dev_mnist
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
-from test.convolutional.convlayers import bias_variable
-from test.convolutional.convlayers import conv2d
-from test.convolutional.convlayers import conv_layer_batch_norm
-from test.convolutional.convlayers import full_layer
-from test.convolutional.convlayers import max_pool_2x2
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -71,12 +57,10 @@ def cnn_model_fn(features, keep_probability, mode):
 
 X_train, Y_train, X_test, Y_test = load_train_and_dev_mnist()
 
-
 x = tf.placeholder(tf.float32, shape=(None, 784))
 y_ = tf.placeholder(tf.float32, shape=(None, 10))
 keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 training_mode = tf.placeholder(tf.bool, name="training_mode_bool")
-
 
 y_conv = cnn_model_fn(x, keep_prob, training_mode)
 
@@ -84,60 +68,40 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_
 
 global_step_tensor = tf.Variable(0, trainable=False, name="global_step")
 starter_learning_rate = 1e-4
-# learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step_tensor, 150000, 0.96)
-learning_rate = tf.Variable(starter_learning_rate, trainable=False)
 
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_step = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
+with tf.name_scope("learning_rate"):
+    # learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step_tensor, 150000, 0.96)
+    learning_rate = tf.Variable(starter_learning_rate, trainable=False)
+    tf.summary.scalar('learning_rate', learning_rate)
+
+with tf.name_scope('train'):
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_step = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
 
 # valMon = ValidationMonitor(x=X_train.T, y=Y_train.T, every_n_steps=300)
 # logHook = LoggingTensorHook(every_n_iter=300, tensors={learning_rate})
 
 # tf.estimator.EstimatorSpec(train_op=train_step, loss=cross_entropy, training_hooks={logHook})
 
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-
-def random_mini_batches(X, Y, mini_batch_size=64):
-    """
-    Produces shuffled list of minibatches
-    :param X:
-    :param Y:
-    :param mini_batch_size:
-    :return: List of tuples (mini_batch_X, mini_batch_Y)
-    """
-    m = X.shape[1]
-    mini_batches = []
-
-    # Shuffle (X, Y)
-    permutation = list(np.random.permutation(m))
-    shuffled_X = X[:, permutation]
-    shuffled_Y = Y[:, permutation].reshape((Y.shape[0], m))
-
-    # Partition (shuffled_X, shuffled_Y). Minus the end case.
-    num_complete_minibatches = math.floor(
-        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
-    for k in range(0, num_complete_minibatches):
-        mini_batch_X = shuffled_X[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
-        mini_batch_Y = shuffled_Y[:, k * mini_batch_size: k * mini_batch_size + mini_batch_size]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-
-    # Handling end case where last mini-batch < mini_batch_size
-    if m % mini_batch_size != 0:
-        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size: m]
-        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size: m]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-
-    return mini_batches
-
+with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_prediction'):
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    with tf.name_scope('accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+tf.summary.scalar('accuracy', accuracy)
 
 saver = tf.train.Saver(max_to_keep=20)
 
+
 with tf.Session() as sess:
+
     sess.run(tf.global_variables_initializer())
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('/tmp/tensorflow/mnist/logs/mnist_with_summaries_cnn/train', sess.graph)
+    test_writer_dev = tf.summary.FileWriter('/tmp/tensorflow/mnist/logs/mnist_with_summaries_cnn/test_dev')
+    test_writer_train = tf.summary.FileWriter('/tmp/tensorflow/mnist/logs/mnist_with_summaries_cnn/test_train')
+
     for epoch in range(500):
         epoch_cost = 0.
         m = X_train.shape[1]
@@ -148,8 +112,11 @@ with tf.Session() as sess:
         for minibatch in minibatches:
             xt = minibatch[0].T
             yt = minibatch[1].T
-
-            sess.run(train_step, feed_dict={x: xt, y_: yt, keep_prob: 0.40, training_mode: True})
+            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            run_metadata = tf.RunMetadata()
+            sess.run(train_step, feed_dict={x: xt, y_: yt, keep_prob: 0.40, training_mode: True},
+                     run_metadata=run_metadata, options=run_options)
+            train_writer.add_run_metadata(run_metadata, tf.train.global_step(sess, global_step_tensor))
 
             minibatch_cost = \
                 sess.run(cross_entropy, feed_dict={x: xt, y_: yt, keep_prob: 1, training_mode: False})
@@ -159,11 +126,13 @@ with tf.Session() as sess:
         if epoch % 5 == 0:
             print("--------------------------------------------------------------------------------------------------")
             print('global_step: %s' % tf.train.global_step(sess, global_step_tensor))
-            dev_accuracy = \
-                sess.run(accuracy, feed_dict={x: X_test.T, y_: Y_test.T, keep_prob: 1.0, training_mode: False})
+            summary, dev_accuracy = \
+                sess.run([merged, accuracy], feed_dict={x: X_test.T, y_: Y_test.T, keep_prob: 1.0, training_mode: False})
+            test_writer_dev.add_summary(summary, tf.train.global_step(sess, global_step_tensor))
             print("EPOCH {}, DEV accuracy {}".format(epoch, dev_accuracy))
-            train_accuracy = \
-                sess.run(accuracy, feed_dict={x: X_train.T, y_: Y_train.T, keep_prob: 1.0, training_mode: False})
+            summary, train_accuracy = \
+                sess.run([merged, accuracy], feed_dict={x: X_train.T, y_: Y_train.T, keep_prob: 1.0, training_mode: False})
+            test_writer_train.add_summary(summary, tf.train.global_step(sess, global_step_tensor))
             print("EPOCH {}, TRAIN accuracy {}".format(epoch, train_accuracy))
             print("EPOCH COST: " + str(epoch_cost))
 
